af1a23dde6cf34420245bdf2ad701aa87be62b28
Rodrigo Davila Castillo <rodrigo.davilacastillo@tamucc.edu>
2024-10-07 02:52:56 -0500

    Chapter 12: Invoice updating/editing functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -33,5 +34,24 @@ export async function createInvoice(formData: FormData) {
     // Ensure the user sees the new invoice on the invoices route by clearing cache
     revalidatePath('/dashboard/invoices');
     // Go back to invoices page
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
