8a972fe144a851451c788ca4dccfaa758d91f302
Rodrigo Davila Castillo <rodrigo.davilacastillo@tamucc.edu>
2024-10-08 23:31:14 -0500

    Added custom titles for dashboard, customers, edit invoice, create invoice, and login pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
     template: '%s | Acme Dashboard',
     default: 'Acme Dashboard',
   },
-  description: 'The official Next.js Course Dashboard, built with App Router.',
+  description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
-}; 
+};
 
 export default function RootLayout({
   children,
